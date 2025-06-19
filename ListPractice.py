5704edcc9e11adc4b94be28573acc1265f3abf3c
MatticusLong <133465199+MatticusLong@users.noreply.github.com>
2025-06-19 18:12:01 -0500

    Adding files to folders

--- ListPractice.py
+++
@@ -1,8 +0,0 @@
-#Practice with lists
-todo = ['Get quarters', 'Do laundry.', 'Take a walk.', 'Get a haircut', 'Make some tea.', 'Complete Lists chapter.', 'Call mom', 'Watch My Hero Academia']
-#Print the fist and second item
-print(todo[0 : 2])
-#using slicing to print the 3rd 4th and 5th items
-print(todo[2 : 5])
-#Trying to show the index error for #9
-print(todo[8 : 9])
